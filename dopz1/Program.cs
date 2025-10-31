1a6d67a1b62c6e9a11ab89f73e7b9984555fe549
Utka8 <nzuk7261@gmail.com>
2025-10-31 23:06:03 +0300

    Часть 2: вывод результата разбиения

--- dopz1/Program.cs
+++ dopz1/Program.cs
@@ -5,7 +5,11 @@ class Program
     {
         string poems = " тучки небесные, вечные странники...";
         char[] div = { ' ', ',', '.' };
-
         string[] parts = poems.Split(div, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine("Результат разбиения строки на части: ");
+        for (int i = 0; i < parts.Length; i++)
+        {
+            Console.WriteLine(parts[i]);
+        }
     }
 }
