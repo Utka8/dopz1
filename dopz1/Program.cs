2ce5ab087c705680b5f1f03b17ce7c3db16b411e
Utka8 <nzuk7261@gmail.com>
2025-10-31 23:04:29 +0300

    Часть 1: объявление переменных и split

--- dopz1/Program.cs
+++ dopz1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string poems = " тучки небесные, вечные странники...";
+        char[] div = { ' ', ',', '.' };
+
+        string[] parts = poems.Split(div, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
